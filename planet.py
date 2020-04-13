5ae46d586b867dc52edb0ec63ac0d6d823561eef
Laurence Liang <laurencel2001@gmail.com>
2020-04-13 12:35:26 -0400

    Create planet.py

---
+++ planet.py
@@ -0,0 +1,12 @@
+class Planet:
+	def __init__(self):
+		self.population = 1
+		self.population_growth = 1.1
+		self.population_list = []
+		
+	def step(self):
+		self.population = self.population * self.population_growth
+		
+	def run(self, iterations):
+		step(iterations)
+		self.population_list.append(self.population)
